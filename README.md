a978a6863d0b8fa182e8f389c79d3f69387a561a
satputesaket <satputesaket@gmail.com>
2024-08-31 11:06:42 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # us-visa-approval-prediction
 
+'''
+bash
+
+
+conda create -n visa python=3.8 -y
+
+conda activate visa     
+
+conda deactivate
+
+
+'''
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us_visa',
+    version='0.1',
+    author='saket',
+    author_email='satputesaket@gmail.com',
+    description='US Visa Application',
+    packages=find_packages()
+)
