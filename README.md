6dde1e701658eb2808cce2fb7496a98c4f0015f3
satputesaket <satputesaket@gmail.com>
2024-09-07 14:41:03 +0530

    Data ingestion code and typo changes

--- README.md
+++ README.md
@@ -11,3 +11,23 @@ conda activate visa
 conda deactivate
 
 ```
+
+
+### workflow
+
+1. constant
+2. config_entity
+3. artfact_entity
+4. component
+5. pipeline
+6. app.py
+
+
+### set env var
+
+```
+bash
+
+export MONGODB_URL="mongodb+srv://<username>:<password>@cluster0.3zbhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+```
--- demo.py
+++ demo.py
@@ -1,11 +1,17 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
 
-try:
-    logging.info("Exception OCCurred");
+# try:
+#     logging.info("Exception OCCurred");
 
-    a= 1/"jdksajd"
+#     a= 1/"jdksajd"
 
-except USvisaException as e:
-    raise USvisaException(e,sys) from e
+# except USvisaException as e:
+#     raise USvisaException(e,sys) from e
+
+from us_visa.pipeline.training_pipeline import TrainPipeline
+
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
